59972e061b94de3e67ba0de8efc62385c144daa5
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2023-07-28 09:03:42 -0500

    revert HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,32 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-interface IHelloWorld {
-    function helloWorld() external view returns (string memory);
+contract HelloWorld {
+    string private text;
+    address public owner;
 
-    function setText(string memory newText) external;
-
-    function transferOwnership(address newOwner) external;
-
-    function getOwner() external returns (address);
-}
-
-contract Greeter {
-    function invokeGreeting(
-        address target
-    ) external view returns (string memory) {
-        return IHelloWorld(target).helloWorld();
+    constructor() {
+        text = "Hello World";
+        owner = msg.sender;
     }
 
-    function setGreeting(address target, string calldata newText) external {
-        IHelloWorld(target).setText(newText);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
     }
 
-    function changeOwner(address target, address newOwner) external {
-        IHelloWorld(target).transferOwnership(newOwner);
+    function helloWorld() public view returns (string memory) {
+        return text;
     }
 
-    function checkOwner(address target) external returns (address) {
-        return IHelloWorld(target).getOwner();
+    function setText(string calldata newText) public onlyOwner {
+        text = newText;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    function getOwner() public view returns (address owner_) {
+        return owner;
     }
 }
