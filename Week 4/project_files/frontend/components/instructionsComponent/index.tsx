3616451bea334fb96977c528c766e7b2cc4ce66b
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2023-08-20 21:40:33 -0500

    add request mint button and myTokenJson to next frontend

--- Week 4/project_files/frontend/components/instructionsComponent/index.tsx
+++ Week 4/project_files/frontend/components/instructionsComponent/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import styles from './instructionsComponent.module.css'
 import { useAccount, useNetwork, useBalance, useSignMessage, useContractRead } from 'wagmi'
+import * as myTokenJson from '@/assets/MyToken.json'
 
 export default function InstructionsComponent() {
     return (
@@ -21,7 +22,6 @@ function PageBody() {
     return (
         <div>
             <WalletInfo></WalletInfo>
-            <TokenAddressFromApi></TokenAddressFromApi>
         </div>
     )
 }
@@ -34,11 +34,7 @@ function WalletInfo() {
             <div>
                 <p>Your account address is {address}</p>
                 <p>Connected to the network {chain?.name}</p>
-                <WalletAction></WalletAction>
                 <WalletBalance address={address}></WalletBalance>
-                <TokenName></TokenName>
-                <TokenBalance address={address}></TokenBalance>
-                <RandomProfile></RandomProfile>
                 <RequestTokens address={address}></RequestTokens>
             </div>
         )
@@ -74,203 +70,86 @@ function WalletBalance(params: { address: any }) {
     )
 }
 
-function WalletAction() {
-    const [signatureMessage, setSignatureMessage] = useState('')
+// function WalletAction() {
+//     const [signatureMessage, setSignatureMessage] = useState('')
 
-    const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage()
-    return (
-        <div>
-            <form>
-                <label>
-                    Enter the message to be signed:
-                    <input
-                        type="text"
-                        value={signatureMessage}
-                        onChange={(e) => setSignatureMessage(e.target.value)}
-                    />
-                </label>
-            </form>
-            <button
-                disabled={isLoading}
-                onClick={() =>
-                    signMessage({
-                        message: signatureMessage,
-                    })
-                }
-            >
-                Sign message
-            </button>
-            {isSuccess && <div>Signature: {data}</div>}
-            {isError && <div>Error signing message</div>}
-        </div>
-    )
-}
-
-function TokenName() {
-    const { data, isError, isLoading } = useContractRead({
-        address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-        abi: [
-            {
-                constant: true,
-                inputs: [],
-                name: 'name',
-                outputs: [
-                    {
-                        name: '',
-                        type: 'string',
-                    },
-                ],
-                payable: false,
-                stateMutability: 'view',
-                type: 'function',
-            },
-        ],
-        functionName: 'name',
-    })
-
-    const name = typeof data === 'string' ? data : 0
-
-    if (isLoading) return <div>Fetching name…</div>
-    if (isError) return <div>Error fetching name</div>
-    return <div>Token name: {name}</div>
-}
-
-function TokenBalance(params: { address: `0x${string}` }) {
-    const { data, isError, isLoading } = useContractRead({
-        address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-        abi: [
-            {
-                constant: true,
-                inputs: [
-                    {
-                        name: '_owner',
-                        type: 'address',
-                    },
-                ],
-                name: 'balanceOf',
-                outputs: [
-                    {
-                        name: 'balance',
-                        type: 'uint256',
-                    },
-                ],
-                payable: false,
-                stateMutability: 'view',
-                type: 'function',
-            },
-        ],
-        functionName: 'balanceOf',
-        args: [params.address],
-    })
-
-    const balance = typeof data === 'number' ? data : 0
-
-    if (isLoading) return <div>Fetching balance…</div>
-    if (isError) return <div>Error fetching balance</div>
-    return <div>Balance: {balance}</div>
-}
-
-function RandomProfile() {
-    const [data, setData] = useState<any>(null)
-    const [isLoading, setLoading] = useState(true)
-
-    useEffect(() => {
-        fetch('https://randomuser.me/api/')
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data.results[0])
-                setLoading(false)
-            })
-    }, [])
-
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
-
-    return (
-        <div>
-            <h1>
-                Name: {data.name.title} {data.name.first} {data.name.last}
-            </h1>
-            <p>Email: {data.email}</p>
-        </div>
-    )
-}
-
-function TokenAddressFromApi() {
-    const [data, setData] = useState<{ address: string }>()
-    const [isLoading, setLoading] = useState(true)
-
-    if (isLoading) return <p>Loading token address from API...</p>
-    if (!data) return <p>No token address information</p>
-
-    return (
-        <div>
-            <p>Token address from API: {data.address}</p>
-        </div>
-    )
-}
-
-function RequestTokens(params: { address: string }) {
-  const [data, setData] = useState<{ result: string }>();
-  const [isLoading, setLoading] = useState(false);
-
-  const body = { address: params.address };
-
-  if (isLoading) return <p>Requesting tokens from API...</p>;
-  if (!data)
-    return (
-      <button
-        onClick={() => {
-          setLoading(true);
-          fetch("http://localhost:3001/mint-tokens", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body),
-          })
-            .then((res) => res.json())
-            .then((data) => {
-              setData(data);
-              setLoading(false);
-            });
-        }}
-      >
-        Request tokens
-      </button>
-    );
-
-  return (
-    <div>
-      <p>Result from API: {data.result ? 'worked' : 'failed'}</p>
-    </div>
-  );
-}
-
-// from class - onLoad
-// function RequestTokens(params: { address: string }) {
-//     const [data, setData] = useState<{ address: string }>()
-//     const [isLoading, setLoading] = useState(true)
-
-//     const body = { address: params.address }
-
-//     useEffect(() => {
-//         fetch('http://localhost:3001/mint-tokens', {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify(body),
-//         })
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 setData(data)
-//                 setLoading(false)
-//             })
-//     }, [])
-//     if (isLoading) return <p>Requesting tokens from API...</p>
-//     if (!data) return <p>Request failed</p>
+//     const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage()
 //     return (
 //         <div>
-//             <p>Result from API: {data.result}</p>
+//             <form>
+//                 <label>
+//                     Enter the message to be signed:
+//                     <input
+//                         type="text"
+//                         value={signatureMessage}
+//                         onChange={(e) => setSignatureMessage(e.target.value)}
+//                     />
+//                 </label>
+//             </form>
+//             <button
+//                 disabled={isLoading}
+//                 onClick={() =>
+//                     signMessage({
+//                         message: signatureMessage,
+//                     })
+//                 }
+//             >
+//                 Sign message
+//             </button>
+//             {isSuccess && <div>Signature: {data}</div>}
+//             {isError && <div>Error signing message</div>}
 //         </div>
 //     )
 // }
 
-// function requestTokens(address: string) {}
+function RequestTokens(params: { address: string }) {
+    const [data, setData] = useState<{
+        result: string
+        tx: string
+        to: string
+        from: string
+        gasUsed: string
+    }>()
+    const [isLoading, setLoading] = useState(false)
+
+    const body = { address: params.address }
+
+    if (isLoading) return <p>Requesting tokens from API...</p>
+    if (!data)
+        return (
+            <button
+                onClick={() => {
+                    setLoading(true)
+                    fetch('http://localhost:3001/mint-tokens', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(body),
+                    })
+                        .then((res) => res.json())
+                        .then((data) => {
+                            setData(data)
+                            setLoading(false)
+                        })
+                }}
+            >
+                Request tokens
+            </button>
+        )
+
+    if (!data.result) {
+        return (
+            <div>
+                <p>Result from API: FAILED!</p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <p>Result from API: WORKED!</p>
+            <p>Transaction Hash: {data.tx}</p>
+            <p>Mint Tokens to: {data.to}</p>
+            <p>Mint Tokens from: {data.from}</p>
+            <p>Gas Used: {data.gasUsed}</p>
+        </div>
+    )
+}
