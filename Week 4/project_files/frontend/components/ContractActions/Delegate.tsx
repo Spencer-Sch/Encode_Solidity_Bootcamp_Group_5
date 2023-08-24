98c075b6050af5e0f2d00712342326b038a43e0c
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2023-08-23 21:31:40 -0500

    add error handling and etherscan link to Delegate.tsx

--- Week 4/project_files/frontend/components/ContractActions/Delegate.tsx
+++ Week 4/project_files/frontend/components/ContractActions/Delegate.tsx
@@ -4,23 +4,27 @@ import * as myTokenJSON from '@/assets/MyToken.json'
 const myTokenABI = myTokenJSON.abi
 
 export default function Delegate(params: { address: string }) {
-    const { config } = usePrepareContractWrite({
-        address: '0xbBAfa95dF21F2AD94f65A71DF988a4284E5E08dD',
-        // address: (process.env.MY_TOKEN_CONTRACT_ADDRESS as `0x${string}`) ?? '',
+    const {
+        config,
+        error: prepareError,
+        isError: isPrepareError,
+    } = usePrepareContractWrite({
+        // address: '0xbBAfa95dF21F2AD94f65A71DF988a4284E5E08dD',
+        address: (process.env.NEXT_PUBLIC_MY_TOKEN_CONTRACT_ADDRESS as `0x${string}`) ?? '',
         abi: myTokenABI,
         functionName: 'delegate',
         args: [params.address],
         chainId: sepolia.id,
         account: params.address as `0x${string}`,
         onError(error) {
-            console.log('usePrepare Error: ', error)
+            console.log('usePrepare DELEGATE Error: ', error)
         },
         onSuccess(data) {
-            console.log('usePrepare Success: ', data)
+            console.log('usePrepare DELEGATE Success: ', data)
         },
     })
 
-    const { data, isLoading: loadingWrite, write } = useContractWrite(config)
+    const { data, isLoading: loadingWrite, error, isError, write } = useContractWrite(config)
     const { isLoading: awaitingTX, isSuccess } = useWaitForTransaction({
         hash: data?.hash,
     })
@@ -36,7 +40,18 @@ export default function Delegate(params: { address: string }) {
                 {awaitingTX ? 'Delegating...' : 'Self Delegate'}
             </button>
             {loadingWrite && <div>Sign Transaction...</div>}
-            {isSuccess && <div>Transaction: {JSON.stringify(data?.hash)}</div>}
+            {isSuccess && (
+                <div>
+                    <p>Delegation Succesful!</p>
+                    <p>Hash: {JSON.stringify(data?.hash)}</p>
+                    <div>
+                        <a target="_blank" href={`https://etherscan.io/tx/${data?.hash}`}>
+                            View On Etherscan
+                        </a>
+                    </div>
+                </div>
+            )}
+            {(isPrepareError || isError) && <div>Error: {(prepareError || error)?.message}</div>}
         </form>
     )
 }
