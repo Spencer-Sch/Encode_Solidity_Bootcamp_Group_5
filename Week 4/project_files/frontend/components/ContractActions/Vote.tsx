40ef684c6a77331ce80c4907f4aaa09537192152
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2023-08-23 21:32:16 -0500

    add error handling and etherscan link to Vote.tsx

--- Week 4/project_files/frontend/components/ContractActions/Vote.tsx
+++ Week 4/project_files/frontend/components/ContractActions/Vote.tsx
@@ -1,4 +1,4 @@
-import { sepolia, useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { sepolia, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
 import * as ballotJson from '@/assets/TokenizedBallot.json'
 import { useState } from 'react'
 import { parseUnits } from 'viem'
@@ -13,26 +13,31 @@ export default function Vote(params: { address: string }) {
     const debouncedProposition = useDebounce(proposition)
     const debouncedAmount = useDebounce(amount)
 
-    // function callContract() {
-    const { config } = usePrepareContractWrite({
-        address: '0x672721258028a7d1BB302dfBE91eEaa8Ec848Ea3',
-        // address: (process.env.TOKENIZED_BALLOT_CONTRACT_ADDRESS as `0x${string}`) ?? '',
+    const {
+        config,
+        error: prepareError,
+        isError: isPrepareError,
+    } = usePrepareContractWrite({
+        // address: '0x672721258028a7d1BB302dfBE91eEaa8Ec848Ea3',
+        address:
+            (process.env.NEXT_PUBLIC_TOKENIZED_BALLOT_CONTRACT_ADDRESS as `0x${string}`) ?? '',
         abi: ballotABI,
         functionName: 'vote',
-        // args: [0, parseUnits('1', 18)],
         args: [debouncedProposition, debouncedAmount],
         chainId: sepolia.id,
         account: params.address as `0x${string}`,
         onError(error) {
-            console.log('usePrepare Error: ', error)
+            console.log('usePrepare VOTE Error: ', error)
         },
         onSuccess(data) {
-            console.log('usePrepare Success: ', data)
+            console.log('usePrepare VOTE Success: ', data)
         },
     })
 
-    const { data, isLoading, isSuccess, write } = useContractWrite(config)
-    // }
+    const { data, isLoading: loadingWrite, error, isError, write } = useContractWrite(config)
+    const { isLoading: awaitingTX, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
 
     function handleAmountConversion(value: string) {
         let amountBN
@@ -50,37 +55,51 @@ export default function Vote(params: { address: string }) {
     }
 
     return (
-        <form>
-            <p>Write is: {write ? 'true' : 'false'}</p>
-            <p>{isLoading}</p>
-            <button disabled={!write} onClick={() => write?.()}>
-                Vote
+        <form
+            onSubmit={(e) => {
+                e.preventDefault()
+                write?.()
+            }}
+        >
+            <button type="submit" disabled={!write || awaitingTX}>
+                {awaitingTX ? 'Voting...' : 'Vote'}
             </button>
             <label htmlFor="proposals">Choose a proposal:</label>
             <select
+                disabled={awaitingTX}
                 required
                 name="proposals"
                 id="proposals"
                 onChange={(e) => setProposition(Number(e.target.value))}
             >
-                {/* <option value="">--Choose Proposal to Vote For</option> */}
                 <option value="0">Prop 0</option>
                 <option value="1">Prop 1</option>
                 <option value="2">Prop 2</option>
             </select>
             <label htmlFor="amount">Choose an amount to vote:</label>
             <select
+                disabled={awaitingTX}
                 required
                 name="amount"
                 id="amount"
                 onChange={(e) => handleAmountConversion(e.target.value)}
             >
-                {/* <option value="">--Choose Vote Amount</option> */}
                 <option value="one">Full Coin</option>
                 <option value="half">Half Coin</option>
             </select>
-            {isLoading && <div>Check Wallet</div>}
-            {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+            {loadingWrite && <div>Sign Transaction...</div>}
+            {isSuccess && (
+                <div>
+                    <p>Vote Succesful!</p>
+                    <p>Hash: {JSON.stringify(data?.hash)}</p>
+                    <div>
+                        <a target="_blank" href={`https://etherscan.io/tx/${data?.hash}`}>
+                            View On Etherscan
+                        </a>
+                    </div>
+                </div>
+            )}
+            {(isPrepareError || isError) && <div>Error: {(prepareError || error)?.message}</div>}
         </form>
     )
 }
